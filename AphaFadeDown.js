199cf4fcf00301b77a4b1102f3c2ff28bcf89260
Ben Keenan <ben@ben.ie>
2014-04-12 00:04:55 +0100

    Initial commit

---
+++ AphaFadeDown.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+public var fadeRate : double = 0.01;
+
+function Start () {
+
+}
+
+function Update () {
+
+	if (renderer.material.color.a > 0)
+	{renderer.material.color.a = renderer.material.color.a + fadeRate;}
+	else
+	{renderer.material.color.a = 0;}
+
+}
---
+++ AphaFadeUpAndDown.js
@@ -0,0 +1,50 @@
+﻿#pragma strict
+
+public var fadeRate : float = 0.01;
+private var fadeUp : boolean = false;
+private var fadeDown : boolean = false;
+
+function Start () {
+	
+	fadeUp = true;
+	
+}
+
+function Update () {
+
+	if (fadeUp)
+	{FadeUp();}
+	if (fadeDown)
+	{FadeDown();}
+}
+
+function FadeUp() {
+	
+
+	if (renderer.material.color.a > 0)
+	{renderer.material.color.a = renderer.material.color.a - fadeRate;}
+	else
+	{
+	renderer.material.color.a = 0;
+	fadeUp = false;
+	}	
+}
+
+function FadeDown() {
+	
+
+	if (renderer.material.color.a < 254.5)
+	{renderer.material.color.a = renderer.material.color.a + fadeRate;}
+	else
+	{
+	renderer.material.color.a = 255;
+	fadeDown = false;
+	}
+	
+}
+
+
+function OnTriggerEnter()
+{
+ 	fadeDown = true;
+}
